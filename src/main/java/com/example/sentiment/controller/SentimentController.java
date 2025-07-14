1a3a0076227c3005023b9ee15a22dca51b4ddcfd
Mo Ray Smandi <mohamedsamandi@rayan.home>
2025-07-14 11:43:04 +0200

    add Promeutheus and Grafana

--- src/main/java/com/example/sentiment/controller/SentimentController.java
+++ src/main/java/com/example/sentiment/controller/SentimentController.java
@@ -2,6 +2,8 @@ package com.example.sentiment.controller;
 
 import com.example.sentiment.model.ReviewRequest;
 import com.example.sentiment.service.SentimentService;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,8 @@ import java.util.Map;
 
 @RestController
 public class SentimentController {
+    @Autowired
+    MeterRegistry meterRegistry;
     private final SentimentService sentimentService;
 
     public SentimentController(SentimentService sentimentService) {
@@ -17,6 +21,7 @@ public class SentimentController {
     @PostMapping("/analyze")
     public ResponseEntity<Map<String, String>> analyze(@RequestBody ReviewRequest reviewRequest) {
         Map<String, String>  sentiment = sentimentService.analyze(reviewRequest.getReview());
+        meterRegistry.counter("review.analyzed.total").increment();
 
         return ResponseEntity.ok(sentiment);
     }
